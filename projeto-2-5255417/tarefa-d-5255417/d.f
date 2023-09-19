2def4212432871615c2fb66cb4e41a801f588b51
delbempedro <pedrodelbemo@gmail.com>
2023-09-19 13:33:18 -0300

    .

--- projeto-2-5255417/tarefa-d-5255417/d.f
+++ projeto-2-5255417/tarefa-d-5255417/d.f
@@ -25,47 +25,76 @@ c     define o número de passos
 c     define o nome do arquivo de saída
       name = 'histograma'
     
-c     define as somas
-      somar1 = 0
-      somar2 = 0
-    
-c     define o loop dos andarilhos
-      do i=1,m
-    
-c       define a posição do andarilho
-        ix = 1000
-        iy = 1000
-c       define o loop de cada andarilho
-        do j=1,n
-                                                     
-c         define a direção do passo
-          a = rand(0)
-          if(a.lt.ap) then
-    
-            ix = ix+1
-    
-          else if(a.lt.2.0e0*ap) then
-    
-            ix = ix-1
-    
-          else if(a.lt.3.0e0*ap) then
-    
-            iy = iy+1
-    
-          else
-    
-            iy = iy-1
-    
-          end if
-    
-        end do
-    
-        ihisto(ix,iy) = ihisto(ix,iy) + 1
-    
-        s = (ix-1000)**2 + (iy-1000)**2
-        somar1 = somar1 + sqrt(s)
-        somar2 = somar2 + s
-    
+c     aloca a memória para salvar os dados do histograma
+      open(unit=1,file=name)
+
+c     escreve 1000 no arquivo que define nas coordenadas x e y de cada partícula
+      do k=1, m
+
+        write(1,*) 1000,1000
+
       end do
+
+c     calcula a entropia a cada ordem 10 de passos
+      ipassos = 1
+      ipassosf = 10
+      do while(ipassosf.le.nmax)
+
+c     define o loop dos andarilhos
+        do ipassos=1,passosf
+
+c     define a posição do andarilho
+          read(1,*) ix,iy
+
+c     define o loop de cada andarilho
+          do j=1,n
+                                                      
+c     define a direção do passo
+            a = rand(0)
+              if(a.lt.ap) then
+
+                ix = ix+1
+
+              else if(a.lt.2.0e0*ap) then
+
+                ix = ix-1
+
+              else if(a.lt.3.0e0*ap) then
+
+                iy = iy+1
+
+              else
+
+                iy = iy-1
+
+              end if
+
+          end do
+
+          ihisto(ix,iy) = ihisto(ix,iy) + 1
+          write(1,*) ix,iy
+
+        end do
+
+c     redefine as variáveis de iteração para continuar os passos de onde pararam
+        ipassos = ipassos*10
+        ipassosf = ipassosf*10
+
+      entropia
+
+      end do
+
+c     fecha a unidade de memória
+      close(1)
       
-      end program    
+      end program
+
+      function entropia()
+
+      do while(ix.le.2000 and iy.le.2000)
+
+
+
+      end do
+
+      end function
