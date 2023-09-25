36e75d77cb611c094a3f39c0f612c7218724ef98
delbempedro <pedrodelbemo@gmail.com>
2023-09-25 11:55:39 -0300

    .

--- projeto-2-5255417/tarefa-d-5255417/d.f
+++ projeto-2-5255417/tarefa-d-5255417/d.f
@@ -11,7 +11,7 @@ c     define as probabilidades
       ap = 0.250e0
     
 c     define o número de passos
-      nmax = 10e2
+      nmax = 1000
     
 c     define o nome do arquivo de saída
       name = 'histograma-5255417'
@@ -22,75 +22,70 @@ c     aloca a memória para salvar os dados do histograma
 c     aloca memória para salvar os dados da entropia
       open(unit=2, file='gráfico-5255417')
 
-c     escreve 0 no arquivo que define nas coordenadas x e y de cada partícula
-      do k=1, n
-
-        write(1,*) 0,0
-
-      end do
-
 c     calcula a entropia a cada ordem 10 de passos
-      ipassos = 0
-      ipassosf = 1
+      ipassos = 1
       do while(ipassosf.le.nmax)
 
-c       limpa o vetor que armazena a posição dos andarilhos
+c       escreve 0 em todas as coordenadas do plano
         ihisto(-1000:1000,-1000:1000) = 0
 
-c     define o loop dos andarilhos
-      do i=1,n
+c       define o loop dos andarilhos
+        do i=1,n
 
-c       define a posição do andarilho
-        ix = 0
-        iy = 0
-c       define os passos possíveis
-        ipx(0) = 0
-        ipx(1) = 0
-        ipx(2) = 0
-        ipx(3) = 0
+c         define a posição do andarilho
+          ix = 0
+          iy = 0
+
+c         define os passos possíveis
+          ipx(0) = 0
+          ipx(1) = 0
+          ipx(2) = 0
+          ipx(3) = 0
       
-c       define o loop de cada andarilho
-        do j=ipassos,ipassosf
+c         define o loop de cada andarilho
+          do j=0,ipassos
                                                       
-c         define a direção do passo
-          a = rand(0)
+c           define a direção do passo
+            a = rand(0)
       
-c         faz a divisão inteira de i por ap
-          int = rand(0)/ap   
+c           faz a divisão inteira de i por ap
+            int = rand(0)/ap   
       
-c         incrementa um passo na direção correspondente a "int"
-          ipx(int) = ipx(int) + 1
+c           incrementa um passo na direção correspondente a "int"
+            ipx(int) = ipx(int) + 1
       
-c         define as coordenadas finais de acordo com quantos passos
-c         foram dados em cada direção
-          ix = ipx(0) - ipx(1)
-          iy = ipx(2) - ipx(3)
+          end do
+
+c           define as coordenadas finais de acordo com quantos passos
+c           foram dados em cada direção
+            ix = ipx(0) - ipx(1)
+            iy = ipx(2) - ipx(3) 
+      
+          ihisto(ix,iy) = ihisto(ix,iy) + 1
+          write(1,*) ix,iy
       
         end do
-      
-        ihisto(ix,iy) = ihisto(ix,iy) + 1
-        write(1,*) ix,iy
-      
-            end do
 
 c       escreve em um arquivo a relação entropia X passos
         an = n
-        write(2,*) ipassosf, entropia(ihisto,an)
-      
-c       redefine as variáveis de iteração para continuar os passos de onde pararam
-        ipassos = ipassosf
-        ipassosf = ipassos + 1
+        write(2,*) ipassos, entropia(ihisto,an)
+
+c       incrementa a quantidade de passos
+        ipassos = ipassos + 1
 
       end do
 
-c     fecha as unidades de memória
-      close(1)
+c     fecha a unidade de memória
       close(2)
+      close(1)
       
       end program
 
       function entropia(ihisto, an)
       dimension ihisto(-1000:1000,-1000:1000)
+
+c     define o tamanho do retículado
+      iret = 10
 
 c     inicia o loop percorrendo todo o "plano"
       i = -1000
@@ -102,8 +97,8 @@ c         define o número de partículas inicial do retículado como 0
           aparticulas = 0
 
 c         percorre cada retículado contabilizando quantas partículas existem no mesmo
-          do k=i,i+10
-            do l=j,j+10
+          do k=i,i+iret
+            do l=j,j+iret
 
 c             soma à quantidade de a quantidade de partículas na posição (k,l)
               aparticulas = aparticulas + ihisto(k,l)
@@ -124,11 +119,11 @@ c             soma a probabilidade devido a um retículado
             
           end if
 
-          j = j + 10
+          j = j + iret
 
         end do
 
-        i = i + 10
+        i = i + iret
 
       end do
 
