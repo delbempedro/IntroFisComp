6ae5ee97be3170f396fb19c26dbd789f4d6f16e5
delbempedro <pedrodelbemo@gmail.com>
2023-10-23 13:12:39 -0300

    melhorando a impressao

--- projeto-3-5255417/tarefa-3-5255417/3.f
+++ projeto-3-5255417/tarefa-3-5255417/3.f
@@ -1,6 +1,7 @@
       program raiz
 
       implicit real*8 (a-h,o-z)
+      dimension vetbd(3,6), vetrn(3,6), vetsec(3,6)
 c     valores exatos -7, 2, 9
 
 c     tolerância para considerar a raiz exata
@@ -20,7 +21,7 @@ c     busca direta:
       do i=1,3
 
 c           imprimi a indicação de qual raiz está sendo calculada
-            write(1,*)"r",i
+            write(1,1)"r",i
 
 c           passo incementado na busca pela raiz
             ap = 0.1d0
@@ -29,105 +30,110 @@ c           inicializa o valor de raizbd para procurar a próxima raiz
             raizbd = raizbd + ap
             j = 1
             do while(j.eq.1)
-c abs(f(raizbd)).ge.tol
+
+c                 verifica se a raiz está dentro do intervalo
                   aux = raizbd + ap
-                  if(f(raizbd)*f(aux).lt.0.0d0) then
+                  if(f(raizbd)*f(aux).le.0.0d0) then
 
-                        ap = ap/2.0d0
-                        x = raizbd + ap
-                        write(1,*)x
-                        k = 1
-                        do while(k.lt.6)
+c                       inicia o loop com as 6 interações da tabela
+                        do k=1,6
 
+c                             divide o intervalo pela metade e imprimi
+c                             a raiz correspondente
+                              ap = ap/2.0d0
                               aux = raizbd + ap
-                              if(f(raizbd)*f(aux).lt.0.0d0) then
-                                    ap = ap/2.0d0
-                                    x = raizbd + ap
-                                    write(*,*)x
-                                    k = k + 1
-                              end if
+                              vetbd(i,k) = aux
 
-                              raizbd = raizbd + ap
+c                             muda o intervalo analisado até que haja
+c                             uma raiz no intervalo
+                              do while(f(raizbd)*f(aux).gt.0.0d0)
+
+                                    raizbd = raizbd + ap
+                                    aux = raizbd + ap
+
+                              end do
+
 
                         end do
                         j = 0
 
                   end if
-            
 
                   raizbd = raizbd + ap
 
             end do
 
-            write(1,*)raizbd
-
       end do
-
-c     define o valor inicial como -10
-      raixrn = -10.0d0
 
 c     Newton-Raphson:
       write(1,*)'Newton-Raphson'
       do i=1,3
 
 c           imprimi a indicação de qual raiz está sendo calculada
-            write(1,*)"r",i
+            write(1,1)"r",i
 
-c           passo incementado na busca pela raiz
-            ap = 0.1d0
+c           define o valor do passo
+            ap = (i-1)*10.0d0
 
-c           atualiza o valor atual
-            raizrn = raizrn + ap
+c           define o valor inicial
+            raizrn = -10.0d0 + ap
+
+c           define o contador de interações
+            icount = 1
 
             do while(abs(f(raizrn)).ge.tol)
-                  
-c                 atualiza o valor antigo
-                  aux = raizrn
 
-c                 atualiza o valor atual
-                  x = rn(rairn,aux)
+c                 atualiza o valor
+                  x = rn(raizrn)
                   raizrn = x
-                  write(1,*)raizrn
+                  vetrn(i,icount) = raizrn
+
+c                 incrementa o contador
+                  icount = icount + 1
 
             end do
 
-            write(1,*)raizrn
-
       end do
-
-c     define o valor inicial como -10
-      raixs = -10.0d0
 
 c     Secante:
       write(1,*)'Secante'
       do i=1,3
 
 c           imprimi a indicação de qual raiz está sendo calculada
-            write(1,*)"r",i
+            write(1,1)"r",i
 
-c           passo incementado na busca pela raiz
-            ap = 0.1d0
+c           define o valor do passo
+            ap = (i-1)*10.0d0
 
-c           atualiza o valor atual
-            raizs = raizs + ap
+c           define o valor inicial
+            raizs = -10.0d0 + ap
+
+c           define o valor anterior
+            aux = -10.0d0 + (ap-1.0d0)
+
+c           define o contador de interações
+            icount = 1
 
             do while(abs(f(raizs)).ge.tol)
-                  
+
+c                 passa o valor atual para x
+                  x = s(raizs,aux)
+
 c                 atualiza o valor antigo
                   aux = raizs
 
 c                 atualiza o valor atual
-                  x = s(raizs,aux)
                   raizs = x
-                  write(1,*)raizs
+                  vetrn(i,icount) = raizs
+
+c                 incrementa o contador
+                  icount = icount + 1
 
             end do
 
-            write(1,*)raizs
-
       end do
 
-1     format(a1,f1.0)
+1     format(a,i1)
 
 c     fecha o arquivo de saida
       close(1)
@@ -143,10 +149,10 @@ c     define a função
       end function
 
 c     define o metodo de newton-raphson
-      real*8 function rn(x,xa)
+      real*8 function rn(x)
       implicit real*8 (a-h,o-z)
 
-      rn = x - f(x)/( (f(x)-f(xa))/(x-xa) )
+      rn = x - f(x)/( 3.0d0*(x**2.0d0) - 8.0d0*x - 59.0d0)
 
       end function
 
@@ -154,6 +160,6 @@ c     define o metodo da secante
       real*8 function s(x,xa)
       implicit real*8 (a-h,o-z)
 
-      s = x - f(x)*( (x-xa)/(f(x)-f(xa)) )
+      s = x - (f(x)*(x-xa))/(f(x)-f(xa))
 
       end function
